035cfa945fe3e08e0175ceb87ec186c42428aa8e
Dhaval Shah <dhaval@alphastack.in>
2022-05-14 18:50:14 +0530

    Adding custom indicator with z index

--- compose-library/src/main/java/com/example/composelibrary/tabs/CustomTabs.kt
+++ compose-library/src/main/java/com/example/composelibrary/tabs/CustomTabs.kt
@@ -2,6 +2,7 @@ package com.example.composelibrary.tabs
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.runtime.Composable
@@ -9,9 +10,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 
 @Preview
 @Composable
@@ -34,7 +37,7 @@ fun CustomTabs() {
         ScrollableTabRow(
             modifier = Modifier.wrapContentWidth(),
             selectedTabIndex = selectedTabIndex.value,
-            contentColor = Color.Red,
+            contentColor = Color.Red.copy(alpha = 0.5f),
             edgePadding = 0.dp,
             tabs = {
                 for (i in 0..10) {
@@ -53,9 +56,15 @@ fun CustomTabs() {
                 TabRowDefaults.Indicator(
                     Modifier
                         .tabIndicatorOffset(tabPosition)
-                        .height(2.dp),
+                        .zIndex(-1f)
+                        .padding(vertical = 4.dp, horizontal = 2.dp)
+                        .clip(RoundedCornerShape(100.dp))
+                        .fillMaxHeight(),
                 )
             },
+            divider = {
+
+            }
         )
     }
 }
