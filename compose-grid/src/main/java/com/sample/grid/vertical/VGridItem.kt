3cf52efd6170f01747073104f76eed23e14ccda5
Dhaval Shah <dhaval@alphastack.in>
2022-05-30 17:09:48 +0530

    Added derived state logic in grid

--- compose-grid/src/main/java/com/sample/grid/vertical/VGridItem.kt
+++ compose-grid/src/main/java/com/sample/grid/vertical/VGridItem.kt
@@ -1,10 +1,13 @@
 package com.sample.grid.vertical
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -13,18 +16,30 @@ import androidx.compose.ui.unit.dp
 @Preview
 @Composable
 private fun PreviewVGridItem() {
-    VGridItem(color = Color.Yellow)
+    VGridItem(
+        color = Color.Yellow,
+        gridItem = GridItem(index = 1, selected = false, id = 1L),
+        onItemClick = {}
+    )
 }
 
 @Composable
 internal fun VGridItem(
     modifier: Modifier = Modifier,
-    color: Color
+    color: Color,
+    gridItem: GridItem,
+    onItemClick: () -> Unit
 ) {
     Box(
-        modifier = modifier.fillMaxWidth()
+        modifier = modifier
+            .fillMaxWidth()
             .heightIn(min = 100.dp)
-            .background(color),
+            .background(color)
+            .clickable { onItemClick.invoke() },
+        contentAlignment = Alignment.Center
     ) {
+        if (gridItem.selected) {
+            Text(text = gridItem.index.toString())
+        }
     }
 }
--- compose-grid/src/main/java/com/sample/grid/vertical/VerticalGridScreen.kt
+++ compose-grid/src/main/java/com/sample/grid/vertical/VerticalGridScreen.kt
@@ -4,10 +4,8 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.GridCells
-import androidx.compose.foundation.lazy.GridItemSpan
-import androidx.compose.foundation.lazy.LazyVerticalGrid
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.lazy.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,6 +24,25 @@ internal fun VerticalGridScreen(
 ) {
     val spacing = 30.dp
 
+    val items = remember {
+        mutableStateOf(getItems())
+    }
+    val selectedItems: MutableState<List<GridItem>> = remember {
+        mutableStateOf(emptyList())
+    }
+    val shownItems = remember(selectedItems.value) {
+
+        derivedStateOf {
+            println("debug_derived: Here")
+            val result = mutableListOf<GridItem>()
+            for (item in items.value) {
+                val index = getIndex(item, selectedItems.value)
+                result.add(item.copy(index = index + 1, selected = index != -1))
+            }
+            result.toList()
+        }
+    }
+
     LazyVerticalGrid(
         modifier = Modifier.fillMaxSize(),
         cells = GridCells.Fixed(3),
@@ -33,14 +50,28 @@ internal fun VerticalGridScreen(
         verticalArrangement = Arrangement.spacedBy(spacing),
         contentPadding = PaddingValues(spacing),
         content = {
-            items(40) { index ->
-                VGridItem(color = getColor(index + 1))
+            itemsIndexed(shownItems.value) { index, item ->
+                VGridItem(
+                    color = getColor(index + 1),
+                    gridItem = item,
+                    onItemClick = {
+                        if (item.selected) {
+                            selectedItems.value = selectedItems.value.deleteItem { it.id == item.id }
+                        } else {
+                            selectedItems.value = selectedItems.value.addItemAtEnd(item)
+                        }
+                    }
+                )
             }
             /*item(span = { GridItemSpan(maxCurrentLineSpan) }) {
                 VGridItem(color = Color.Yellow)
             }*/
         }
     )
+}
+
+private fun getIndex(item: GridItem, items: List<GridItem>): Int {
+    return items.indexOfFirst { it.id == item.id }
 }
 
 private fun getColor(index: Int): Color {
@@ -51,4 +82,14 @@ private fun getColor(index: Int): Color {
         1 -> Color.Red.copy(alpha = 0.2F)
         else -> Color.Red.copy(alpha = 1F)
     }
+}
+
+private fun getItems(): List<GridItem> {
+    return listOf(
+        GridItem(index = 0, selected = false, id = 1L),
+        GridItem(index = 0, selected = false, id = 2L),
+        GridItem(index = 0, selected = false, id = 3L),
+        GridItem(index = 0, selected = false, id = 4L),
+        GridItem(index = 0, selected = false, id = 5L),
+    )
 }
